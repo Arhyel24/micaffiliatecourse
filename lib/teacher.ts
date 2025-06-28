0b4703efc9c2d324ff3cf4d0febff1e28058d7ed
Enoch Philip Dibal <arhyelphilip024@gmail.com>
2025-06-28 16:15:47 +0100

    lll

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -2,6 +2,5 @@ export function isTeacher(email?: string | null) {
   const teacher = process.env.NEXT_PUBLIC_TEACHER_ID as string;
 
   const teacher2 = process.env.NEXT_PUBLIC_TEACHER_ID_2 as string;
-  
   return email === teacher || email === teacher2;
 }
